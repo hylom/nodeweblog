b8c3e79be0dce6e42c06d9c604e41fbbea11919f
hylom <hylom@hylom.net>
2013-03-25 01:48:53 +0900

    add accessLog file support, remove '/admin/' routing

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
+
 var MemcachedStore = require('connect-memcached')(express);
-
 var config = require('./config');
-
 var app = express();
 
 app.configure(function(){
@@ -21,7 +21,25 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+
+  // setting for logging
+  // when nodeweblog does not run under root privilleges,
+  // use 'dev' style logging.
+  if (process.env.NODE_ENV === 'development' || 
+      (process.getuid() !== 0)) {
+    app.use(express.logger('dev'));
+  } else {
+    // if config.accessLog is exists, output logs to the file.
+    var logStream;
+    if (config.logFile) {
+      logStream = fs.createWriteStream(config.accessLog, {mode: 'a'});
+    }
+    app.use(express.logger({
+      format: 'default',
+      stream: logStream || process.stdout
+    }));
+  }
+
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser(config.cookieHash));
@@ -50,7 +68,7 @@ app.configure('development', function(){
 app.get('/admin/login', routes.admin.login);
 app.post('/admin/login', routes.admin.login);
 app.get('/admin/logout', routes.admin.logout);
-app.get('/admin/', routes.admin.index);
+// app.get('/admin/', routes.admin.index);
 app.get('/admin/account', routes.admin.account);
 app.post('/admin/account', routes.admin.account);
 
@@ -64,7 +82,7 @@ app.get('/page/:page', routes.index);
 app.get('/:url', routes.single);
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   if ((config.nodeUser !== undefined) && (process.getuid() == 0) ){
     process.setgid(config.nodeUser.gid);
     process.setuid(config.nodeUser.uid);
